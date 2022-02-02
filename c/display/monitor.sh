9ba74bf7985dde9cf0836cf4959eb4de3063b76a
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2022-02-02 11:52:59 +0000

    Put display code in separate folder

---
+++ c/display/monitor.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ minicom -b 115200 -o -D /dev/ttyACM0
+
--- c/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir build
-cd build
-cmake ..
-make -j4
-openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program epd.elf verify reset"
-
