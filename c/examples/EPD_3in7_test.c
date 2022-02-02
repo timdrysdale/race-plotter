45c9434f59b33e29c447dc9c2070ca0dc54f31ab
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2022-02-02 00:12:43 +0000

    Remove stale code

--- c/examples/EPD_3in7_test.c
+++ c/examples/EPD_3in7_test.c
@@ -51,65 +51,6 @@ int EPD_3in7_test(void)
         return -1;
     }
 
-    printf("Paint_NewImage\r\n");
-    Paint_NewImage(BlackImage, EPD_3IN7_WIDTH, EPD_3IN7_HEIGHT, 90, WHITE);
-    Paint_SetScale(2);
-    Paint_Clear(WHITE);
-
-#if 0 // Try large font
-
-    //Paint_DrawString_EN(10, 90, "! ,", &Font200, GRAY1, BLACK);
-    Paint_DrawString_EN(10, 10, "&", &Font200, WHITE, BLACK);
-
-    EPD_3IN7_1Gray_Display(BlackImage);
-
-    DEV_Delay_ms(1000);
-
-    Paint_Clear(WHITE);
-
-    Paint_DrawString_EN(10, 90, "%&", &Font200, WHITE, BLACK);
-
-    EPD_3IN7_1Gray_Display(BlackImage);
-
-    DEV_Delay_ms(1000);
-
-#endif 
-
-#if 0 // Drawing on the image, partial display
-    //1.Select Image
-    printf("SelectImage:BlackImage\r\n");   
-    Paint_SelectImage(BlackImage);
-    Paint_SetScale(4);
-    Paint_Clear(WHITE);
-
-    // 2.Drawing on the image
-    printf("Drawing:BlackImage\r\n");
-    Paint_DrawPoint(10, 80, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
-    Paint_DrawPoint(10, 90, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
-    Paint_DrawPoint(10, 100, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
-    Paint_DrawLine(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawLine(70, 70, 20, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawRectangle(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(80, 70, 130, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawCircle(45, 95, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawCircle(105, 95, 20, WHITE, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawLine(85, 95, 125, 95, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawLine(105, 75, 105, 115, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawString_EN(10, 0, "G'day!", &Font16, BLACK, WHITE);
-    Paint_DrawString_EN(10, 20, "Greetings world", &Font12, WHITE, BLACK);
-    Paint_DrawNum(10, 33, 123456789, &Font12, BLACK, WHITE);
-    Paint_DrawNum(10, 50, 987654321, &Font16, WHITE, BLACK);
-    Paint_DrawString_EN(10, 150, "HELLO THERE!!", &Font24, BLACK, GRAY1);
-    Paint_DrawString_EN(10, 175, "GOO with white background", &Font24, WHITE, GRAY2);
-    Paint_DrawString_EN(10, 200, "BAR with white background", &Font24, WHITE, GRAY3);
-    Paint_DrawString_EN(10, 225, "FOO white background", &Font24, WHITE, GRAY4);
-    printf("EPD_Display\r\n");
-    EPD_3IN7_4Gray_Display(BlackImage);
-    DEV_Delay_ms(4000);
-#endif
-
-#if 1 // partial update, just 1 Gray mode
-
     const char *a[5];
     a[0] = "\")$";
     a[1] = "\")&";
@@ -122,8 +63,10 @@ int EPD_3in7_test(void)
     EPD_3IN7_1Gray_Init();       //init 1 Gray mode
     EPD_3IN7_1Gray_Clear();
 
+
     for (i = 0; i <5; i++) { 
-    Paint_NewImage(BlackImage, 270, 450, 90, WHITE);
+
+    Paint_NewImage(BlackImage, EPD_3IN7_WIDTH, EPD_3IN7_HEIGHT, 90, WHITE);
     Paint_SelectImage(BlackImage);
     Paint_SetScale(2);
     Paint_Clear(WHITE);
@@ -136,7 +79,7 @@ int EPD_3in7_test(void)
 
     if ( i > 0) {
     Paint_DrawRectangle(240,3,260,38, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-	}
+        }
     if  (i > 1) {
        Paint_DrawRectangle(265,3,285,38, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
     } 
@@ -150,15 +93,12 @@ int EPD_3in7_test(void)
        Paint_DrawRectangle(340,3,365,38, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
     } 
 
-    EPD_3IN7_1Gray_Display_Part(BlackImage, 8, 8, 278, 458); // Xstart must be a multiple of 8
-
-
-    //EPD_3IN7_1Gray_Display_Part(BlackImage, 8, 208, 208, 408); 
+    EPD_3IN7_1Gray_Display(BlackImage);
     DEV_Delay_ms(1000);
-}
-    
 
-#endif
+}
+
+
 
     EPD_3IN7_4Gray_Init();
     printf("Clear...\r\n");
